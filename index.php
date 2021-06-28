4521f13cbb13af27642a2b17157747f32f634dc5
domingo79 <andria140779@yahoo.it>
2021-06-28 20:25:28 +0200

    style<<

--- index.php
+++ index.php
@@ -14,29 +14,46 @@ require __DIR__ . '/server/database.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title>Book Shop On Line</title>
 </head>
 
 <body>
+    <div class="container-xl d-flex flex-wrap justify-content-between">
+
+        <?php foreach ($books as $value) : ?>
+
+            <div class="card " style="width: 28rem;">
+                <img style="width: 440px;" src="https://picsum.photos/id/237/400/200" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h2 class="card-title"> <?= $value->getTitle(); ?></h2>
+                    <p class="card-text"><?= $value->getSubTitle(); ?></p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Autore: <?= $value->getAuthor(); ?></li>
+                    <li class="list-group-item">Prezzo: € <?= $value->getPrice(); ?></li>
+                    <li class="list-group-item">Editore: <?= $value->editor; ?> </li>
+                    <li class="list-group-item">Lingua : <?= $value->language; ?> </li>
+                    <li class="list-group-item">Copertina flessibile : <?= $value->flexible_cover; ?> </li>
+                    <li class="list-group-item">ISBN : <?= $value->ISBN_10; ?> </li>
+                    <li class="list-group-item">Peso articolo : <?= $value->articole_weight; ?> </li>
+                    <li class="list-group-item">Dimensioni : <?= $value->size; ?> </li>
+                    <li class="list-group-item">Valutazione : <?= $value->customer_reviews; ?> </li>
+                </ul>
+                <div class="card-body">
+                    <button type="button" class="btn btn-outline-success">Acquista</button>
+                    <button type="button" class="btn btn-outline-primary">altri Correlati</button>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+
+    </div>
+    </div>
+    </div>
 
 
-    <?php foreach ($books as $value) : ?>
-        <h1>Titolo: <?= $value->getTitle(); ?></h1>
-        <h2>Sottotitolo: <?= $value->getSubTitle(); ?></h2>
-        <h3>Autore: <?= $value->getAuthor(); ?></h3>
-        <h4>Prezzo: € <?= $value->getPrice(); ?></h4>
 
-        <h2>Dettaglio del prodotto</h2>
-
-        <p>Editore: <?= $value->editor; ?> </p>
-        <p>Lingua : <?= $value->language; ?> </p>
-        <p>Copertina flessibile : <?= $value->flexible_cover; ?> </p>
-        <p>ISBN : <?= $value->ISBN_10; ?> </p>
-        <p>Peso articolo : <?= $value->articole_weight; ?> </p>
-        <p>Dimensioni : <?= $value->size; ?> </p>
-        <p>Valutazione : <?= $value->customer_reviews; ?> </p>
-
-    <?php endforeach; ?>
 </body>
 
 </html>
