1821aa17f1bb08a5a665fa4d76af921a257893b3
domingo79 <andria140779@yahoo.it>
2021-06-28 17:12:44 +0200

    strutturato percorsi e cartelle

---
+++ classes/BookShop.php
@@ -0,0 +1,52 @@
+<?php
+
+class BooksShop
+{
+
+    public $title;
+    public $sub_title;
+    public $author;
+    protected $price;
+
+    public function __construct(string $title, string $sub_title, string $author, float $price)
+    {
+        $this->title = $title;
+        $this->sub_title = $sub_title;
+        $this->author = $author;
+        $this->price = $price;
+    }
+}
+
+class Book extends BooksShop
+{
+    public $editor;
+    public $language;
+    public $flexible_cover;
+    public $ISBN_10;
+    public $articole_weight;
+    public $size;
+    public $customer_reviews;
+
+    public function __construct(
+        string $title,
+        string $sub_title,
+        string $author,
+        float $price,
+        string $editor,
+        string $language,
+        int $flexible_cover,
+        string $ISBN_10,
+        int $articole_weight,
+        string $size,
+        float $customer_reviews
+    ) {
+        parent::__construct($title, $sub_title, $author, $price);
+        $this->editor = $editor;
+        $this->language = $language;
+        $this->flexible_cover = $flexible_cover;
+        $this->ISBN_10 = $ISBN_10;
+        $this->articole_weight = $articole_weight;
+        $this->size = $size;
+        $this->customer_reviews = $customer_reviews;
+    }
+}
---
+++ server/database.php
@@ -0,0 +1,44 @@
+<?php
+include __DIR__ . '../classes/BookShop.php';
+
+$normalEnglish = new Book(
+    'NormaL English',
+    'Il mio metodo fast, fun and fantastic',
+    'Norma Celetti',
+    17.10,
+    'Mondadori (15 giugno 2021)',
+    'Italiano',
+    264,
+    '8804741716',
+    620,
+    '15.8x2.2x22.3 cm',
+    4.9
+);
+
+$laCanzoneDiAchille = new Book(
+    'La Canzone di Achille',
+    'Miti saghe e leggende',
+    'Madeline Miller',
+    10.45,
+    'Marsilio (10 gennaio 2019)',
+    'Italiano',
+    382,
+    '8831780980',
+    270,
+    '20.2 x 2.4 x 13.2 cm',
+    4.7
+);
+
+$ilNervoVago = new Book(
+    'Il nervo Vago',
+    'Come Stimolare e Attivare il Nervo Vago. Esercizi per Alleviare Ansia e Depressione, Prevenire Infiammazioni e Controllare le Emozioni',
+    'Madeline Miller',
+    14.97,
+    'Independently published (29 marzo 2021)',
+    'Italiano',
+    132,
+    '8730299467',
+    160,
+    '12.7 x 0.76 x 20.32 cm',
+    4.4
+);
